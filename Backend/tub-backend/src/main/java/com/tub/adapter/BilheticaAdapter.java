a477deef7115870066537c2ec260a43552b2afd6
renatosousa1707 <renatosousa1707@gmail.com>
2026-03-15 00:27:02 +0000

    .

---
+++ Backend/tub-backend/src/main/java/com/tub/adapter/BilheticaAdapter.java
@@ -0,0 +1,10 @@
+package com.tub.adapter;
+
+import java.util.List;
+import com.tub.model.Validation;
+
+public interface BilleticaAdapter {
+
+    List<Validation> getValidations();
+
+}
---
+++ Backend/tub-backend/src/main/java/com/tub/adapter/WavecomAdapter.java
@@ -0,0 +1,10 @@
+package com.tub.adapter;
+
+import java.util.List;
+import com.tub.model.PassengerCount;
+
+public interface WavecomAdapter {
+
+    List<PassengerCount> getPassengerCounts();
+
+}
