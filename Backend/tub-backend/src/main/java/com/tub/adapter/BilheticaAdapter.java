8d7e6fba7e34646aa5b0a369ce14e0c26eabae89
mariana-196 <mariana.o.s196@gmail.com>
2026-03-16 22:57:42 +0000

    erros corrigidos

--- Backend/tub-backend/src/main/java/com/tub/adapter/BilheticaAdapter.java
+++ Backend/tub-backend/src/main/java/com/tub/adapter/BilheticaAdapter.java
@@ -3,7 +3,7 @@ package com.tub.adapter;
 import java.util.List;
 import com.tub.model.Validation;
 
-public interface BilleticaAdapter {
+public interface BilheticaAdapter {
 
     List<Validation> getValidations();
 
--- Backend/tub-backend/src/main/java/com/tub/controller/BilheticaController.java
+++ Backend/tub-backend/src/main/java/com/tub/controller/BilheticaController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.List;
 import java.util.Arrays;
 
 @RestController
