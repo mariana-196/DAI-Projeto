c4bdcc4a3c811c22300f876de82441243679c962
renatosousa1707 <renatosousa1707@gmail.com>
2026-05-15 23:25:10 +0100

    parte do uc2

--- Backend/tub-backend/src/main/java/com/tub/p6_auditoria/controller/ControloConsultaAuditoria.java
+++ Backend/tub-backend/src/main/java/com/tub/p6_auditoria/controller/ControloConsultaAuditoria.java
@@ -1,8 +1,9 @@
 package com.tub.p6_auditoria.controller;
 
 import com.tub.p6_auditoria.model.RegistoAuditoria;
-
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpHeaders; 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
@@ -39,5 +40,36 @@ public class ControloConsultaAuditoria {
     @PostMapping("/logs")
     public RegistoAuditoria criarLog(@RequestBody RegistoAuditoria registo) {
         return auditService.guardarLog(registo);
+    }
+
+    @GetMapping("/exportar")
+    public ResponseEntity<String> exportarRelatorioCsv(
+            @RequestParam(required = false) String utilizador,
+            @RequestParam(required = false) String acao,
+            @RequestParam(required = false) String modulo,
+            @RequestParam(required = false) String nivel,
+            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime dataInicio,
+            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime dataFim
+    ) {
+        List<RegistoAuditoria> logs = auditService.pesquisarLogs(utilizador, acao, modulo, nivel, dataInicio, dataFim);
+    
+        StringBuilder csv = new StringBuilder();
+        csv.append("Data;Utilizador;Acao;Modulo;IP;Nivel;Detalhe\n"); // Cabeçalho (usamos ';' para o Excel em PT abrir bem)
+    
+        for (RegistoAuditoria log : logs) {
+            csv.append(log.getTimestamp()).append(";")
+                .append(log.getUtilizador()).append(";")
+                .append(log.getAcao()).append(";")
+                .append(log.getModulo()).append(";")
+                .append(log.getIpOrigem()).append(";")
+                .append(log.getNivel()).append(";")
+                .append(log.getDetalhe()).append("\n");
+        }
+
+    // 3. Devolve o ficheiro para o browser fazer download
+        return ResponseEntity.ok()
+                .header(org.springframework.http.HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=relatorio_auditoria_tub.csv")
+                .header(org.springframework.http.HttpHeaders.CONTENT_TYPE, "text/csv; charset=UTF-8")
+                .body(csv.toString());
     }
 }
