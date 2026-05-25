e21fca88ef684c7dca00a4be6924eda2f896918e
mariana-196 <mariana.o.s196@gmail.com>
2026-05-26 00:43:20 +0100

    mais auditoria

--- Backend/tub-backend/src/main/java/com/tub/p1_autenticacao/model/SessaoAutenticada.java
+++ Backend/tub-backend/src/main/java/com/tub/p1_autenticacao/model/SessaoAutenticada.java
@@ -13,7 +13,7 @@ public class SessaoAutenticada {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, unique = true, length = 100)
+    @Column(nullable = false, unique = true, length = 768)
     private String token;
 
     @ManyToOne
